7afcd935d5a3aa88adf04b1b4b3fdfb891fbc25c
Ryan HO <ryan.ho@fdmgroup.com>
2022-11-09 23:30:55 +0000

    Using pytest BDD to validate an API is working, test is parametized and data is injected from examples

---
+++ tests/step_defs/test_service.py
@@ -0,0 +1,44 @@
+"""
+This module contains step definitions for service.feature.
+It uses the requests package:
+http://docs.python-requests.org/
+"""
+
+import requests
+
+from pytest_bdd import scenarios, parsers, given, then, parsers
+
+#Scenarios
+
+scenarios("../features/service.feature")
+
+# Shared Variables
+
+DUCKDUCKGO_API = "https://api.duckduckgo.com/"
+
+
+
+CONVERTERS = {
+    "phrase": str
+}
+
+#Given Steps
+
+
+@given(parsers.parse('the DuckDuckGo API is queried with {phrase}'),target_fixture="ddg_response", converters=dict(phrase=str))
+#@given('the DuckDuc kGo API is queried with "<phrase>"',converters=dict(phrase=str)) #only the given can be used as fixture
+def ddg_response(phrase):
+    params = {'q': phrase, 'format':'json'}
+    response = requests.get(DUCKDUCKGO_API,params = params)
+    return response
+
+# Then Steps
+
+@then(parsers.parse('the response contains result for {phrase}'))
+def ddg_response_contents(ddg_response, phrase):
+    #A more comprehensive test would check 'RelatedTopics' for matching phrases
+    assert phrase.lower() == ddg_response.json()['Heading'].lower()
+
+@then(parsers.parse('the response status code is "{code:d}"'))
+def ddg_response_code(ddg_response,code):
+    assert ddg_response.status_code == code
