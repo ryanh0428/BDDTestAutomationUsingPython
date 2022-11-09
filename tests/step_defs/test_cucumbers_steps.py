959e4fd9cb3400fbcd596ff4c2cd964836ee3fe1
Ryan HO <ryan.ho@fdmgroup.com>
2022-11-09 10:41:53 +0000

    using partial function to simplify code

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,3 +1,4 @@
+from functools import partial
 from pytest_bdd import scenarios, parsers, given, when, then
 
 from cucumber_basket import CucumberBasket
@@ -17,22 +18,25 @@ EXTRA_TYPES={
     'Number':int
 }
 
+#partial function will cause the Gerkin statment fail to recognise the respective method
+parse_num = partial(parsers.cfparse, extra_types= EXTRA_TYPES)
 
-@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', EXTRA_TYPES), target_fixture='basket')
+
+@given(parse_num('the basket has "{initial:Number}" cucumbers'), target_fixture='basket')
 def fixture_target_basket(initial): # become the fixture value that the result will be injected when the step call this fixture
     return CucumberBasket(initial_count=initial)
 
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', EXTRA_TYPES))
+@when(parse_num('"{some:Number}" cucumbers are added to the basket'))
 def add_cucumbers(basket, some):
     basket.add(some)
 
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', EXTRA_TYPES))
+@when(parse_num('"{some:Number}" cucumbers are removed from the basket'))
 def remove_cucumbers(basket, some):
     basket.remove(some)
 
 
-@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', EXTRA_TYPES))
+@then(parse_num('the basket contains "{total:Number}" cucumbers'))
 def basket_has_total(basket, total):
     assert basket.count == total
